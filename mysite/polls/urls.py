82d4baebfe472e5fe817c45e90caa5a6bb36dda9
Shivam Singh <shivamsingh2681@gmail.com>
2025-06-30 15:27:57 +0530

    Add index view to polls app

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),  <- We will add this back in a future assignment
+    path('', views.index, name='index'),
     path('owner', views.owner, name='owner'),
 ] 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("This is the polls index.")
+
 def owner(request: HttpRequest) -> HttpResponse:
     response = HttpResponse()
     response.write("Hello, world. 5fc4279e is the polls index.")
