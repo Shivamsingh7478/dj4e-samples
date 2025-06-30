dbcd9600ee425af7a3595dcec32a1dc1d6a38df1
Shivam Singh <shivamsingh2681@gmail.com>
2025-06-30 22:04:07 +0530

    Add script to create autograder user

--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class CatList(TemplateView):
+    template_name = "cats/cat_list.html"
