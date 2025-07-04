63d4970e8516b972452eca82c61e37b08be79969
Shivam Singh <shivamsingh2681@gmail.com>
2025-07-04 15:24:26 +0530

    All

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,5 @@
-from django.urls import reverse_lazy
+from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 from .models import Ad
-from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
 class AdListView(OwnerListView):
     model = Ad
@@ -14,15 +13,12 @@ class AdCreateView(OwnerCreateView):
     model = Ad
     fields = ['title', 'price', 'text']
     template_name = "ads/ad_form.html"
-    success_url = reverse_lazy('ads:all')
 
 class AdUpdateView(OwnerUpdateView):
     model = Ad
     fields = ['title', 'price', 'text']
     template_name = "ads/ad_form.html"
-    success_url = reverse_lazy('ads:all')
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
-    template_name = "ads/ad_confirm_delete.html"
-    success_url = reverse_lazy('ads:all') 
+    template_name = "ads/ad_confirm_delete.html" 
