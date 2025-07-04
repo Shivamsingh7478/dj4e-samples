de24526377525ae7a0eae0a3e5736296b0523f36
Shivam Singh <shivamsingh2681@gmail.com>
2025-07-04 12:29:46 +0530

    All

--- mysite/cats/urls.py
+++ mysite/cats/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.cats_root, name='all'),
+    path('main/create/', views.CatCreate.as_view(), name='cat_create'),
+    path('main/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
+    path('main/<int:pk>/delete/', views.CatDelete.as_view(), name='cat_delete'),
+    path('breed/', views.BreedList.as_view(), name='breed_list'),
+    path('breed/create/', views.BreedCreate.as_view(), name='breed_create'),
+    path('breed/<int:pk>/update/', views.BreedUpdate.as_view(), name='breed_update'),
+    path('breed/<int:pk>/delete/', views.BreedDelete.as_view(), name='breed_delete'),
 ] 
--- mysite/cats/views.py
+++ mysite/cats/views.py
@@ -1,53 +1,71 @@
 from django.http import HttpResponse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect
 from .models import Cat, Breed
 
 def index(request):
     return HttpResponse("Cats app is working!")
 
+# Custom view for /cats/
+def cats_root(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                auth_login(request, form.get_user())
+                return redirect('all')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'cats/login_on_cats.html', {'form': form})
+    # If authenticated, show the cat list
+    return CatList.as_view()(request)
+
 # Cat Views
-class CatList(ListView):
+class CatList(LoginRequiredMixin, ListView):
     model = Cat
     template_name = 'cats/cat_list.html'
     context_object_name = 'cat_list'
 
-class CatCreate(CreateView):
+class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
     fields = '__all__'
     template_name = 'cats/cat_form.html'
     success_url = reverse_lazy('all')
 
-class CatUpdate(UpdateView):
+class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     fields = '__all__'
     template_name = 'cats/cat_form.html'
     success_url = reverse_lazy('all')
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     template_name = 'cats/cat_confirm_delete.html'
     success_url = reverse_lazy('all')
 
 # Breed Views
-class BreedList(ListView):
+class BreedList(LoginRequiredMixin, ListView):
     model = Breed
     template_name = 'cats/breed_list.html'
     context_object_name = 'breed_list'
 
-class BreedCreate(CreateView):
+class BreedCreate(LoginRequiredMixin, CreateView):
     model = Breed
     fields = '__all__'
     template_name = 'cats/breed_form.html'
     success_url = reverse_lazy('breed_list')
 
-class BreedUpdate(UpdateView):
+class BreedUpdate(LoginRequiredMixin, UpdateView):
     model = Breed
     fields = '__all__'
     template_name = 'cats/breed_form.html'
     success_url = reverse_lazy('breed_list')
 
-class BreedDelete(DeleteView):
+class BreedDelete(LoginRequiredMixin, DeleteView):
     model = Breed
     template_name = 'cats/breed_confirm_delete.html'
     success_url = reverse_lazy('breed_list') 
